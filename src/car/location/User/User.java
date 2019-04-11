b4b352ff794689f22ef641c77d0465baec3931ed
Hnada Mohamed <hnadamohamed18@gmail.com>
2019-04-11 15:05:08 +0200

    initial commit

---
+++ src/car/location/User/User.java
@@ -0,0 +1,27 @@
+package car.location.User;
+
+public class User {
+	
+	int idUser;
+	String CIN;
+	String state;
+	public int getIdUser() {
+		return idUser;
+	}
+	public void setIdUser(int idUser) {
+		this.idUser = idUser;
+	}
+	public String getCIN() {
+		return CIN;
+	}
+	public void setCIN(String cIN) {
+		CIN = cIN;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+
+}
---
+++ src/car/location/client/Bill.java
@@ -0,0 +1,37 @@
+package car.location.client;
+
+import java.util.Date;
+
+public class Bill {
+
+	int idBill;
+	int idContrat;
+	int idClient;
+	Date dateBill;
+	public int getIdBill() {
+		return idBill;
+	}
+	public void setIdBill(int idBill) {
+		this.idBill = idBill;
+	}
+	public int getIdContrat() {
+		return idContrat;
+	}
+	public void setIdContrat(int idContrat) {
+		this.idContrat = idContrat;
+	}
+	public int getIdClient() {
+		return idClient;
+	}
+	public void setIdClient(int idClient) {
+		this.idClient = idClient;
+	}
+	public Date getDateBill() {
+		return dateBill;
+	}
+	public void setDateBill(Date dateBill) {
+		this.dateBill = dateBill;
+	}
+	
+	
+}
